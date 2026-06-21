a30a40a10c6f6d43402981da00fb605b88eaea79
tnhdarkrai1457@gmail.com <phucthinh9112005@gmail.com>
2026-06-22 00:36:07 +0700

    Fix typescript errors for Vercel build

--- src/components/Section3.tsx
+++ src/components/Section3.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { Network, TrendingDown, TrendingUp } from 'lucide-react';
+import { TrendingDown, TrendingUp } from 'lucide-react';
 
 gsap.registerPlugin(ScrollTrigger);
 
